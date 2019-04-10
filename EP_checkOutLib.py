0c88800f8b6af3e41d87505f2df4587802d102af
Li Jimmy <351793645@qq.com>
2019-04-10 09:34:19 +0800

    amond io

--- EP_checkOutLib.py
+++ EP_checkOutLib.py
@@ -21,25 +21,39 @@ import requests, time, json
 
 
 
-def checkOutWorkStatus(pageArg = '1', pagesizeArg = '20'):
-	# 调用EP_coreAPILib模块的EP_getStationList接口，获取停车场信息
-	data = EP_coreAPILib.EP_getStationList(pageArg = '1', pagesizeArg = '20')
-	if data['statusSubContent']['content'] == '':
-		return statusCode.checkSubCode('F','api is error')
-	stationLists = data['statusSubContent']['content']['lists']
+# def checkOutWorkStatus(pageArg = '1', pagesizeArg = '20'):
+# 	# 调用EP_coreAPILib模块的EP_getStationList接口，获取停车场信息
+# 	data = EP_coreAPILib.EP_getStationList(pageArg = '1', pagesizeArg = '20')
+# 	if data['statusSubContent']['content'] == '':
+# 		return statusCode.checkSubCode('F','api is error')
+# 	stationLists = data['statusSubContent']['content']['lists']
 
-	if stationLists == []:
-		return statusCode.checkSubCode('F','the list of station is empty')
-	for i in stationLists:
-		if 'alive' in stationLists.keys() and 'station_id' in stationLists.keys():
-			# 创建一个station_id和alive的哈希表
-			dataContent = {}
-			dataContent[i['station_id']] = i['alive']
-	# 将上述哈希表当做statusSubContent返回
-	return  statusCode.checkSubCode('F',dataContent)
+# 	if stationLists == []:
+# 		return statusCode.checkSubCode('F','the list of station is empty')
+# 	dataContent = {}
+# 	statusStation = {
+# 		False:'掉线',
+# 		True:'在线'
+# 	}
+# 	for i in stationLists:
+# 		if 'alive' in i and 'station_id' in i:
+# 			if i['alive'] not in statusStation.keys():
+# 				return statusCode.checkSubCode('F','the alive not in False and Ture')
+# 			# 创建一个station_id和alive的哈希表	
+# 			dataContent[i['station_id']] = statusStation[i['alive']]
+	
+
+# 	# 将上述哈希表当做statusSubContent返回
+# 	return  statusCode.checkSubCode('T',dataContent)
 
 
-print checkOutWorkStatus()
+# # print checkOutWorkStatus()
+
+#statusStatW = checkOutWorkStatus()['statusSubContent']
+
+# print statusStatW[2641]
+
+
 # 输出结果
 # {
 # 	'statusBaseMessage': 'success', 
@@ -52,3 +66,34 @@ print checkOutWorkStatus()
 # }
 	
 
+def checkOutWorkingStatus(stationIdArg, pageArg = '1', pagesizeArg = '20'):
+	# 调用EP_coreAPILib模块的EP_getStationList接口，获取停车场信息
+	data = EP_coreAPILib.EP_getStationList(pageArg = '1', pagesizeArg = '20')
+	if data['statusSubContent']['content'] == '':
+		return statusCode.checkSubCode('F','api is error')
+	stationLists = data['statusSubContent']['content']['lists']
+	if stationLists == []:
+		return statusCode.checkSubCode('F','the list of station is empty')
+	dataContent = {}
+
+	statusStationDict = {
+		False:'掉线',
+		True:'在线'
+	}
+
+	for i in stationLists:
+		if 'alive' in i and 'station_id' in i:
+			if i['alive'] not in statusStationDict.keys():
+				return statusCode.checkSubCode('F','the alive not in False and Ture')
+			# 创建一个station_id和alive的哈希表	
+			dataContent[i['station_id']] = statusStationDict[i['alive']]
+	
+	statusStationHash = dataContent[stationIdArg]
+	# 将上述哈希表当做statusSubContent返回
+	return  statusCode.checkSubCode('T',statusStationHash)
+
+
+print checkOutWorkingStatus(2641)
+# {'statusSubCode': 0, 'statusSubMessage': 'success', 'statusSubContent': '掉线'}
+print checkOutWorkingStatus(2641)['statusSubContent']
+# 掉线
